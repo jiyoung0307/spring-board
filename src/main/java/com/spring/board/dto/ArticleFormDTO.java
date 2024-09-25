bd1d615126e86111a3b035fdece1a1e8cf47db0e
zy_zero <jiyoungjang0307@gmail.com>
2024-09-25 20:47:13 +0900

    refactor:Refactored constructor and toString methods to use Lombok annotations.

--- src/main/java/com/spring/board/dto/ArticleFormDTO.java
+++ src/main/java/com/spring/board/dto/ArticleFormDTO.java
@@ -10,21 +10,6 @@ public class ArticleFormDTO {
     private String title;   // 제목을 받을 필드
     private String content; // 내용을 받을 필드
 
-    /* 전송받을 제목과 내용을 필드에 저장하는 생성자 추가 -> @AllArgsConstructor */
-/*    public ArticleFormDTO(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }*/
-
-    /* 데이터를 잘 받았는지 확인할 toString() 추가 -> @ToString */
-/*    @Override
-    public String toString() {
-        return "ArticleFormDTO{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }*/
-
     /* DTO -> Entity */
     public Article toEntity() {
         return new Article(null, title, content);
--- src/main/java/com/spring/board/entity/Article.java
+++ src/main/java/com/spring/board/entity/Article.java
@@ -1,10 +1,16 @@
 package com.spring.board.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor  // Article() 생성자를 대체하는 어노테이션 추가
+@NoArgsConstructor   // 기본 생성자를 생성
+@ToString            // toString() 메서드를 대체하는 어노테이션 추가
 @Entity
 public class Article {
-    @Id             // 엔티티의 대푯값 지정
+    @Id   // 엔티티의 대푯값 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성 기능 추가
     private Long id;
 
@@ -15,19 +21,19 @@ public class Article {
     private String content;
 
     /* Article 생성자 추가 */
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
+//    public Article(Long id, String title, String content) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//    }
 
     /* toString() 추가 */
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Article{" +
+//                "id=" + id +
+//                ", title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 }
