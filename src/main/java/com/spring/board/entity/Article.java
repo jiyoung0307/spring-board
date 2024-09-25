fc327d67b2ad49ba9c52968d0b7fb240156919fc
zy_zero <jiyoungjang0307@gmail.com>
2024-09-25 22:19:02 +0900

    refactor:Completed link and redirect setup.

--- src/main/java/com/spring/board/entity/Article.java
+++ src/main/java/com/spring/board/entity/Article.java
@@ -2,12 +2,14 @@ package com.spring.board.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor  // Article() 생성자를 대체하는 어노테이션 추가
 @NoArgsConstructor   // 기본 생성자 추가 어노테이션 추가
 @ToString            // toString() 메서드를 대체하는 어노테이션 추가
+@Data                // Getter Setter
 @Entity
 public class Article {
     @Id   // 엔티티의 대푯값 지정
@@ -19,4 +21,5 @@ public class Article {
 
     @Column
     private String content;
+
 }
