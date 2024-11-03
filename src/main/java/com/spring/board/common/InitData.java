cbdceaf5dcff5fd634cebbe29d0f730af4960e5b
zy_zero <jiyoungjang0307@gmail.com>
2024-11-03 22:32:28 +0900

    feat:Implement Comment entity and repository

--- src/main/java/com/spring/board/common/InitData.java
+++ src/main/java/com/spring/board/common/InitData.java
@@ -2,8 +2,10 @@ package com.spring.board.common;
 
 import com.spring.board.entity.Article;
 import com.spring.board.entity.Coffee;
+import com.spring.board.entity.Comment;
 import com.spring.board.repository.ArticleRepository;
 import com.spring.board.repository.CoffeeRepository;
+import com.spring.board.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -18,6 +20,7 @@ public class InitData implements ApplicationListener<ApplicationReadyEvent> {
 
     private final ArticleRepository articleRepository;
     private final CoffeeRepository coffeeRepository;
+    private final CommentRepository commentRepository;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
@@ -25,6 +28,16 @@ public class InitData implements ApplicationListener<ApplicationReadyEvent> {
         articles.add(new Article(1L, "가가가가", "1111"));
         articles.add(new Article(2L, "나나나나", "2222"));
         articles.add(new Article(3L, "다다다다", "3333"));
+
+        articles.add(new Article(4L, "당신의 인생 영화는?", "댓글 고"));
+        articles.add(new Article(5L, "당신의 소울 푸드는?", "댓글 고고"));
+        articles.add(new Article(6L, "당신의 취미는?", "댓글 고고고"));
+
+        // 저장한 Article 객체를 불러와서 사용
+        Article article4 = articles.get(3);
+        Article article5 = articles.get(4);
+        Article article6 = articles.get(5);
+
 
         // 데이터베이스에 저장
         articleRepository.saveAll(articles);
@@ -37,5 +50,22 @@ public class InitData implements ApplicationListener<ApplicationReadyEvent> {
 
         // 데이터베이스에 저장
         coffeeRepository.saveAll(coffee);
+
+        List<Comment> comments = new ArrayList<>();
+        comments.add(new Comment(1L, article4, "Park", "굿 윌 헌팅"));
+        comments.add(new Comment(2L, article4, "Kim", "아이 엠 샘"));
+        comments.add(new Comment(3L, article4, "Choi", "쇼생크 탈출"));
+
+        comments.add(new Comment(4L, article5, "Park", "치킨"));
+        comments.add(new Comment(5L, article5, "Kim", "샤브샤브"));
+        comments.add(new Comment(6L, article5, "Choi", "초밥"));
+
+        comments.add(new Comment(7L, article6, "Park", "조깅"));
+        comments.add(new Comment(8L, article6, "Kim", "유튜브 시청"));
+        comments.add(new Comment(9L, article6, "Choi", "독서"));
+
+        // 데이터베이스에 저장
+        commentRepository.saveAll(comments);
+
     }
 }
---
+++ src/main/java/com/spring/board/entity/Comment.java
@@ -0,0 +1,27 @@
+package com.spring.board.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 자동으로 1씩 증가
+    private Long id;    // 대표키
+
+    @ManyToOne  // Comment 엔티티와 Article 엔티티를 다대일 관계로 설정
+    @JoinColumn(name = "article_id")    // 외래키 생성, Article 엔티티의 기본키(id)와 매핑
+    private Article article;    // 해당 댓글의 부모 게시물
+
+    @Column // 해당 필드를 테이블의 속성으로 매핑
+    private String nickName;    // 댓글을 단 사람
+
+    @Column // 해당 필드를 테이블의 속성으로 매핑
+    private String body;    // 댓글 본문
+}
